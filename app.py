54ab11ec6c4dad63772c98f64c1224250ef4327f
szid2006 <sz.szabosyd@gmail.com>
2026-01-02 22:58:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -107,3 +107,14 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
 
+import traceback
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(
+        host="0.0.0.0",
+        port=port,
+        debug=True
+    )
+
+
