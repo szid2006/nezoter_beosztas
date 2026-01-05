0694ecc201167c9e382de110a5e608485adde186
szid2006 <sz.szabosyd@gmail.com>
2026-01-05 21:53:04 +0100

    Update rules.py

--- rules.py
+++ rules.py
@@ -1,19 +1,13 @@
 def is_available(worker, show_datetime):
     for entry in worker.unavailable_dates:
-
-        # időintervallum (start, end)
         if isinstance(entry, tuple):
             start, end = entry
             if start <= show_datetime <= end:
                 return False
-
-        # csak dátum
         else:
             if entry == show_datetime.date():
                 return False
-
     return True
-
 
 def ek_allowed(worker, role, ek_in_role):
     if worker.is_ek and role.name == "Jolly joker":
